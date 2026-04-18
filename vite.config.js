cfac8c77cd4245e249f3a16bc4e995a1aa1d17ff
Abdul Kabir <abdulkabir0717@gmail.com>
2026-04-18 23:53:31 +0530

    🥲

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Portfolio/",   // ⚠️ MUST match your repo name
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
